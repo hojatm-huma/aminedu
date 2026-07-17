8d558db544cff90732460c57b42d301ee3fd4635
hojatm-huma <hojat.modaresizadeh@huma.com>
2026-07-17 08:52:45 +0100

    add readme

--- student-fe/app/layout.tsx
+++ student-fe/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Vazirmatn } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const vazirmatn = Vazirmatn({
+  variable: "--font-vazirmatn",
+  subsets: ["latin", "arabic"],
 });
 
 export const metadata: Metadata = {
@@ -23,11 +19,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="fa">
+      <body className={`${vazirmatn.variable} antialiased`}>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
